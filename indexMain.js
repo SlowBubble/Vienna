7c1e92e0949e6e172a40c4b61a2a86c8f6a3f833
SlowBubble <chorhanglam@gmail.com>
2020-07-30 17:26:55 -0400

    Add files via upload

---
+++ indexMain.js
@@ -0,0 +1,73 @@
+import {RecordStateMgr} from './audio/manageState.js';
+import * as pubSub from './utils/pubSub.js'
+import * as banner from './utils/banner.js';
+import { KeyboardShortcuts } from './keyboardShortcuts.js';
+import { ActionMgr } from './actionMgr.js';
+import { WaveFormDrawer } from './audio/drawWaveForm.js';
+import { WaveFormMgr } from './audio/computeWaveForm.js';
+import { MouseShortcuts } from './mouseShortcuts.js';
+
+
+window.onload = _ => {
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
+  const audioCtx = new AudioContext({sampleRate: 44100});
+  const [waveFormPub, waveFormSub] = pubSub.make();
+  const [pointerChangePub, pointerChangeSub] = pubSub.make();
+  const [recorderStoppedPub, recorderStoppedSub] = pubSub.make();
+  const [audioChunkPub, audioChunkSub] = pubSub.make();
+
+  const microphoneRecordingsHtml = document.getElementById('microphone-recordings');
+  const currentTimeHtml = document.getElementById('current-time');
+  pointerChangeSub(time => {
+    currentTimeHtml.textContent = `${Math.floor(time / 100) / 10}s`;
+  });
+
+  const stateMgr = new RecordStateMgr(
+    audioCtx, pointerChangePub, recorderStoppedPub, recorderStoppedSub,
+    audioChunkPub, audioChunkSub,
+    microphoneRecordingsHtml);
+  const eBanner = banner.setup();
+  const actionMgr = new ActionMgr(eBanner, stateMgr);
+  new KeyboardShortcuts(actionMgr);
+  new MouseShortcuts(actionMgr);
+
+  new WaveFormMgr(pointerChangeSub, stateMgr, waveFormPub);
+  new WaveFormDrawer(waveFormSub, document.getElementById('wave-form-canvas'), document.getElementById('right-wave-form-canvas'));
+
+  // For debugging
+  window.stateMgr = stateMgr;
+
+  const fileInput = document.getElementById('audio-file-input');
+  fileInput.onchange = _ => {
+    if (fileInput.files.length < 1) {
+      return;
+    }
+    const blob = fileInput.files[0];
+    console.log(blob.name);
+    const fileNameParts = blob.name.split('.');
+    stateMgr.setFileFormat(fileNameParts[fileNameParts.length - 1]);
+    // This is a hack to upload by faking a recording process.
+    stateMgr.prepareToRecord();
+
+    // audioChunkPub(blob);
+    // This is just a rough under-estimate; an over-estimate is bad due to impl detail.
+    const bytesPerMs = 4;
+    const msPerChunk = 200;
+    const bytesPerChunk = bytesPerMs * msPerChunk;
+    const numChunks = Math.ceil(blob.size / bytesPerChunk);
+    for (let chunkIdx = 0; chunkIdx < numChunks; chunkIdx++) {
+      audioChunkPub(blob.slice(chunkIdx * bytesPerChunk, (chunkIdx + 1) * bytesPerChunk));
+    }
+
+    recorderStoppedPub();
+    fileInput.value = '';
+
+  };
+
+};
+
+
+
+
+
+
